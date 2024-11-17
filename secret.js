bb50ed7626867757d18105b883cd4068fa123b2b
Alicia Desjardins <aliciadesjardins1@gmail.com>
2024-11-17 12:15:53 -0800

    Add the secret encrypt function from the NodeJS project into the website

---
+++ secret.js
@@ -0,0 +1,23 @@
+function encrypt(msg) {
+    let newMessage = "";
+    for (let i = 0; i < msg.length; i++) {
+        const char = msg.charCodeAt(i) // Get the ASCII code
+        const isLowerCase = (char >= 97 && char <= 122);
+        const isUpperCase = (char >=65 && char <= 90);
+        if (isLowerCase || isUpperCase) {
+            const asciiConvertion = isLowerCase?97:65;
+            const alphaOrder = char - asciiConvertion; // ASCII code to letter number form 0 to 25
+            const newAlphaOrder = 25 - alphaOrder; // Apply the secret code
+            newMessage = newMessage + String.fromCharCode(newAlphaOrder + asciiConvertion); // Bring back the letter number to ASCII code
+        }
+        else {
+            newMessage += msg[i];
+        }
+    }
+    return newMessage;
+}
+
+function setsecretinhtml(){
+    const answer = encrypt(document.getElementById('secret_input').value)
+    document.getElementById('secret_ouput').innerText = answer
+}
